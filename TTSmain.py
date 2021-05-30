94da89d14a852484380ea20a35003ee41d390896
m1nus0ne <Voldemar7455@Gmail.com>
2021-05-31 00:29:46 +0500

    reworked

--- TTSmain.py
+++
@@ -1,5 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-
-engine.runAndWait()
-print(help(dir()))
--- graph.py
+++ graph.py
@@ -1,74 +1,60 @@
+import os
+
+
+import kivy
 from kivy.uix.button import Button
-from  kivy.uix.textinput import TextInput
+from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.core.window import Window
-
+from kivy.core.audio import SoundLoader
 import pyttsx3
-import pygame
-import os
-import glob
 
-
-pygame.init()
 engine = pyttsx3.init()
-count=0
-sc = pygame.display.set_mode((1, 1))
-
-
-
-
-
-def to_file(text,name):
-    engine.save_to_file(text,'Audio/'+name+'.wav')
-    engine.runAndWait()
-
+kivy.require('2.0.0')
 
 class ttsApp(App):
+
     def build(self):
-        mbl=BoxLayout(orientation='vertical',
-                      padding=[20,20,20,20])
+        mbl = BoxLayout(orientation='vertical',
+                        padding=[20, 20, 20, 20])
 
-        bl=BoxLayout(padding=[20,20,20,20],
-                     spacing=40,height=.1)
-        # Window.bind(on_request_close=self.clear)
-        self.ti=TextInput(height=.9)
+        bl = BoxLayout(padding=[20, 20, 20, 20],
+                       spacing=40, height=.1)
 
-        b_conv=Button(text='Конвектировать')
-        b_conv.bind(on_press=self.f_conv)
-
-        b_pause=Button(text='Pause')
-        b_pause.bind(on_press=self.f_pause
-                     )
-        b_play=Button(text='Play')
-        b_play.bind(on_press=self.f_play)
+        self.ti = TextInput(height=.9)
+        self.sound = SoundLoader.load('converted.wav')
+        self.time = None
+        convertbutton = Button(text='Convert',on_press = self.convert)
+        pausebutton = Button(text='Pause', on_press = self.f_pause)
+        playbutton = Button(text='Play', on_press = self.f_play)
 
         mbl.add_widget(self.ti)
-        bl.add_widget(b_play)
-        bl.add_widget(b_conv)
-        bl.add_widget(b_pause)
+        bl.add_widget(playbutton)
+        bl.add_widget(convertbutton)
+        bl.add_widget(pausebutton)
         mbl.add_widget(bl)
 
         return mbl
-    def f_conv(self,instance):
-        global count
-        to_file(self.ti.text,str(count))
-        pygame.mixer.music.load('Audio/'+str(count)+'.wav')
-        pygame.mixer.music.play()
-        count+=1
+
+    def convert(self, obj):
+        engine.save_to_file(self.ti.text,'converted.wav')
+        engine.runAndWait()
+        self.sound = SoundLoader.load('converted.wav')
+        os.remove('converted.wav')
 
 
-    def f_pause(self,instance):
-        pygame.mixer.music.pause()
-    def f_play(self,instance):
-        pygame.mixer.music.unpause()
-    # def clear(self,instance):
-    #     files = glob.glob('/Audio/*')
-    #     for f in files:
-    #         print(1)
-    #         os.remove(f)
-    #     print(1)
-    #     return False
+    def f_pause(self, obj):
+        if self.sound!=None:
+            self.sound.stop()
+
+
+
+    def f_play(self, obj):
+        if self.sound != None:
+            self.sound.play()
+
+
+
 if __name__ == '__main__':
     app = ttsApp()
     app.run()
